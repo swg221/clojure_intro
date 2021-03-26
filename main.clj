d681c4462eca948c90e563d98081434191a4bd8b
swg221 <swg221@lehigh.edu>
2021-03-26 16:43:56 +0000

    Initial commit

---
+++ main.clj
@@ -0,0 +1,22 @@
+(def question-7 ((or + -) 4 5)
+)
+(def question-1 10)
+
+
+(def x 10)
+    (if (= x 10) 
+    (def question-2 1)
+    (def question-2 2)
+)
+
+
+
+
+;(def question-2 
+ ; (
+
+  ;;)
+;)
+
+println question-2
+
